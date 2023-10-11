897b7945f928a3e3a1b780253401df55d97b7b60
Mael <mael.cuny@gmail.com>
2023-10-11 04:16:03 -0700

    first code in C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
-void print_header(){
-	printf("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+int main() {
+   puts("Programming is like building a multilingual puzzle");
+   return 0;
 }
