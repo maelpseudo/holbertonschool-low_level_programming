53b25f43f97e09c9d8e96b09f2297fdf29ab65ed
Mael <mael.cuny@gmail.com>
2023-10-17 03:37:09 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Main - Write a function that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+}
