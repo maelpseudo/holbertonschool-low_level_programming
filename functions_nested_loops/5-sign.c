ab62b175f01477fbce543348141cf0af8d509cb8
Mael <mael.cuny@gmail.com>
2023-10-17 13:59:28 -0700

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > '0')
 	{
-		printf("+");
 		return (1);
+		printf("+");
 	}
 	if (n == 0)
 	{
-		printf("0");
 		return (0);
+		printf("0");
 	}
 	else
 	{
-		printf("-");
 		return (-1);
+		printf("-");
 	}
 }
