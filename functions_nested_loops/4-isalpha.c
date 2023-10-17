1e826f7efb8cae50e70686d8f0042d268974090e
Mael <mael.cuny@gmail.com>
2023-10-17 09:47:42 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Main - Write a function that checks for alphabetic character.
+ *
+ * Return: 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
