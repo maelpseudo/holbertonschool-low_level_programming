425ee78ff104c40cdcad3e47a67645771fff1ee9
Mael <mael.cuny@gmail.com>
2023-10-18 04:21:23 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,17 +1,11 @@
+#include <stdio.h>
 #include "main.h"
-#include<stdio.h>
-#include<stdlib.h>
- 
-void times_table(void)
-{
-    int fibonacci[30];
-    fibonacci[0] = 0;
-    fibonacci[1] = 1;
-    short compteur;
- 
-    for(compteur = 2; compteur < 30; compteur ++)
-    {
-         fibonacci[compteur] = fibonacci[compteur - 2] + fibonacci[compteur - 1];
-         printf("%d", fibonacci[compteur]);
-    }
+
+void times_table(void) {
+	int i;
+
+	for (i = 0; i <= 10; i++) 
+	{
+		printf("9 x %d = %d\n", i, 9 * i);
+	}
 }
