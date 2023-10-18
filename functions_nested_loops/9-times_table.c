2dde3a02e91afe64bc053fc8d6fadc1e7079b752
Mael <mael.cuny@gmail.com>
2023-10-18 05:03:59 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,20 +2,18 @@
 #include "main.h"
 
 void times_table(void) {
-	int i, j;
-	for (i = 0; i <= 9; i++) {
-		for (j = 0; j <= 9; j++) {
-			int product = i * j;
-			if (j != 9) {
-				_putchar('0' + product);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('0' + product);
-			}
-		}
-		_putchar('\n');
-	}
+    int i, j;
+
+    for (i = 0; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
+            int product = i * j;
+            if (j != 9) {
+                putchar('0' + product);
+                putchar(' ');
+            } else {
+                putchar('0' + product);
+            }
+        }
+        putchar('\n');
+    }
 }
