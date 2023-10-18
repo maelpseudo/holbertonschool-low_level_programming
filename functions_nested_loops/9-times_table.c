6843f6e0e7198f3083980640bda153835741fd1a
Mael <mael.cuny@gmail.com>
2023-10-18 04:17:46 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,22 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- *
- *
- *
- */
-
+#include<stdio.h>
+#include<stdlib.h>
+ 
 void times_table(void)
 {
-	int nbren=1,i,som=0;
-
-	for(i=1;i<=max;i++)
-	{
-		printf("%d\n",nbren);
-		som=som+nbren;
-		nbren=nbren+som;
-	}
-	system("pause");
-	return(0);
+    int fibonacci[30];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    short compteur;
+ 
+    for(compteur = 2; compteur < 30; compteur ++)
+    {
+         fibonacci[compteur] = fibonacci[compteur - 2] + fibonacci[compteur - 1];
+         printf("%d", fibonacci[compteur]);
+    }
 }
