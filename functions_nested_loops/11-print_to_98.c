ffe7755dd66c5a71e16c938f83948f3013f662b5
Mael <mael.cuny@gmail.com>
2023-10-18 05:49:13 -0700

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_to_98(int n) {
+    if (n <= 98) {
+        for (int i = n; i <= 98; i++) {
+            printf("%d", i);
+            
+            if (i < 98) {
+                printf(", ");
+            }
+        }
+        printf("\n");
+    } else {
+        printf("n is greater than 98. No numbers to print.\n");
+    }
+}
