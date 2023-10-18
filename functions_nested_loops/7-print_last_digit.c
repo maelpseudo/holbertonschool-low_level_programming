8a2f9565859bb84434e49817da4429c856361091
Mael <mael.cuny@gmail.com>
2023-10-18 02:49:16 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n - input
+ * Return: last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+	lastDigit = n % 10;
+	return (lastDigit);
+}	
