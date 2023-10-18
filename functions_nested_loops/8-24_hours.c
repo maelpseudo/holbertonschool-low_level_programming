001c12d8b70994ccb205ad3a6833d8052b7753c4
Mael <mael.cuny@gmail.com>
2023-10-18 03:41:26 -0700

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	for (int h = 0; h < 24; h++) {
+		for (int m = 0; m < 60; m++) {
+			printf("%02d:%02d\n", h, m);
+		}
+	}
+}
