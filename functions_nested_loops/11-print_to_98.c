c1816e10af32b5b272097f5d84b89a6d8e94cb7a
Mael <mael.cuny@gmail.com>
2023-10-18 05:50:07 -0700

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,8 +8,9 @@
  */
 
 void print_to_98(int n) {
+	int i;
     if (n <= 98) {
-        for (int i = n; i <= 98; i++) {
+        for (i = n; i <= 98; i++) {
             printf("%d", i);
             
             if (i < 98) {
