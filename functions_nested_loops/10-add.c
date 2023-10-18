a1a1233d8861ff3396313a09cc70389c986862f9
Mael <mael.cuny@gmail.com>
2023-10-18 05:40:39 -0700

    10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
+ * add - function that adds two integers and return the result
  *
- *
- *
+ * result: return sum
  */
 
 int add(int a, int b)
 {
 	int sum;
 
-	sum = a + b;      
+	sum = a + b;
 	return (sum);
 }
