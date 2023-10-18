9dc2364ea1f4af9616e72aca4842139279667a4e
Mael <mael.cuny@gmail.com>
2023-10-18 03:46:04 -0700

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,18 +2,20 @@
 #include <stdio.h>
 
 /**
+ * jack-bauer - print all minutes from 00:00 to 23:59
  *
- *
- *
+ * result: none
  */
 
 void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h < 24; h++) {
-		for (m = 0; m < 60; m++) {
-			printf("%02d:%02d\n", h, m);
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			printf ("%02d:%02d\n", h, m);
 		}
 	}
 }
