9867adf8be5f3f9244da4019038d65c68851e9a1
Mael <mael.cuny@gmail.com>
2023-10-18 05:12:34 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,12 +4,12 @@
 void times_table(void) {
     int i, j;
 
-    for (i = 0; i <= 9; i++) {
+    for (i = 1; i <= 9; i++) {
         for (j = 0; j <= 9; j++) {
             if (j == 9) {
                 printf("%2d", i * j);
             } else {
-                printf(" %2d, ", i * j);
+                printf("0, %2d, ", i * j);
             }
         }
         printf("\n");
