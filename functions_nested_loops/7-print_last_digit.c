dbb12e18c2636197df804b0386ae4987b08b795f
Mael <mael.cuny@gmail.com>
2023-10-18 03:12:37 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,21 @@
  * Return: last digit of n
  */
 
+int main(void)
+{
+	int lastDigit;
+}
+
 int print_last_digit(int n)
 {
-	int lastDigit
+	int lastDigit;
 	lastDigit = n % 10;
-
-	if (lastDigit > 0)
-	{
-		_putchar(lastDigit);
-	}
-	else if (lastDigit < 0)
+	
+	if (lastDigit < 0)
 	{
 		lastDigit = (lastDigit * (-1));
-		_putchar(lastDigit * (-1));
 	}
+
+	_putchar('0' + lastDigit);
+	return (lastDigit);
 }	
