215e9df3d8c585483b7d70e2f985e78e059e4c2a
Mael <mael.cuny@gmail.com>
2023-10-20 07:19:12 -0700

    2

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0 (Success)
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i >= 9; i++)
+	{
+		printf("%d\n", i);
+		return (0);
+	}
+}
