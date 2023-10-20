cefcacaeecdadf49ddd1d85502625f608f5cac28
Mael <mael.cuny@gmail.com>
2023-10-20 10:12:04 -0700

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * print_square - a function that prints a square
+ * @size: input integer
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				putchar (35);
+			}
+			putchar ('\n');
+		}
+	}
+	else
+		putchar ('\n');
+}
