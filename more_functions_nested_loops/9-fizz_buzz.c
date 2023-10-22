d2800a63153b18a01ba5fac0c6cb16615e6af305
Mael <mael.cuny@gmail.com>
2023-10-22 07:54:11 -0700

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,32 @@ int main()
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if (i != 100)
 		{
-			printf("Fizz ");
-		}
-		else if ((i % 5) == 0)
+
+		if ((i % 3) && (i % 5)  == 0)
 		{
-			printf("Buzz ");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d ", i);
+			if ((i % 3) == 0)
+			{
+				printf("Fizz ");
+			}
+			else if ((i % 5) == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+		}
+		else
+		{
+			printf("Buzz");
 		}
 	}
 	return (0);
