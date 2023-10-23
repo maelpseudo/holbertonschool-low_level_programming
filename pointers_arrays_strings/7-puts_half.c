321b8099c36c93268ef852bd9d07cdfc9ab74df7
maelpseudo <mael.cuny@gmail.com>
2023-10-23 16:44:21 +0200

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,19 @@ void puts_half(char *str)
 {
 int i;
 int length = strlen(str);
+if ((length/2) % 2 == 0)
+{
+for (i = length/2 + 1; i < length; i++)
+{
+putchar(str[i]);
+}
+}
+else
+{
 for (i = length/2; i < length; i++)
 {
 putchar(str[i]);
+}
 }
 putchar('\n');
 }
