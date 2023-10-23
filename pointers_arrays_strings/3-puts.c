b0e551dd6be7e73bafa116d1dc435309d0ed410d
maelpseudo <mael.cuny@gmail.com>
2023-10-23 10:40:38 +0200

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * 
- * 
- * 
+ * _puts - function that print a string
+ * @str: input integer
+ * return: void
 */
 
 void _puts(char *str)
 {
-    printf("%s\n", str);
+printf("%s\n", str);
 }
