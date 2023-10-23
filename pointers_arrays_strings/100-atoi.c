74eaf63faa9b67658a179ed184486ec7a78d7314
maelpseudo <mael.cuny@gmail.com>
2023-10-23 21:39:16 +0200

    10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 
+ * 
+ * 
+*/
+
+int _atoi(char *s)
+{
+    int i;;
+    int var;
+    int length = strlen(s);
+
+    for (i = 0; i < length; i++)
+    {
+        var =+ s[i];
+    }
+}
