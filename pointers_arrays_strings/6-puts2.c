764fa196ac66c56558e3589dd813f7410145e713
maelpseudo <mael.cuny@gmail.com>
2023-10-23 16:28:43 +0200

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /**
- *
- *
- *
+ * puts2 - function that print 1/2 character 
+ * @str: input integer
+ * return: void
 */
 
 void puts2(char *str)
@@ -13,12 +13,18 @@ void puts2(char *str)
 
 int i;
 int length = strlen(str);
-
+if (length == 0)
+{
+    putchar('\n');
+}
+else
+{
 for (i = 0; i <= length; i++)
 {
 if (i % 2 == 0)
 {
     putchar(str[i]);
+}
 }
 }
 putchar('\n');
