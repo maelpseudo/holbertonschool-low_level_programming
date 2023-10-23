9a9c37d4d8864ab869da0e0b45385264b4ad1e17
maelpseudo <mael.cuny@gmail.com>
2023-10-24 00:01:10 +0200

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,17 +13,23 @@ int _atoi(char *s) {
     int length = strlen(s);
     int i, result = 0;
     int sign = 1;
+    int started = 0;
 
     for (i = 0; i < length; i++) {
         if (s[i] == '-') {
             if (isdigit(s[i + 1])) {
                 sign = -1;
+                started = 1;
             }
         } else if (isdigit(s[i])) {
             result = result * 10 + (s[i] - '0');
+            started = 1;
         } else if (s[i] == '+') {
-        } else {
-            continue;
+            if (started) {
+                break;
+            }
+        } else if (started) {
+            break;
         }
     }
 
