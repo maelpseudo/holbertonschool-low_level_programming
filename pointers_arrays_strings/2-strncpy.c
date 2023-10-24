4590ad2c67e0238a3aefd305146d4e08e0bba3fa
maelpseudo <mael.cuny@gmail.com>
2023-10-24 16:25:09 +0200

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,24 +4,27 @@
 #include <stddef.h>
 
 /**
- * 
- * 
- * 
- * 
+ * _strncpy - function that copy a string with max (n)
+ * @dest: pointer to a char
+ * @src: pointer to a char
+ * @n: input integer
+ * Return: @dest
 */
 
 char *_strncpy(char *dest, const char *src, int n)
 {
-    int i;
+int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
 
-    while (i < n) {
-        dest[i] = '\0';
-        i++;
-    }
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
 
-    return dest;
+return dest;
 }
