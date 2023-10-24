7f836eacc97cb3eda2d36a661a6d3ce343dda5aa
maelpseudo <mael.cuny@gmail.com>
2023-10-24 14:06:35 +0200

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i, j;
+    int length = strlen(*dest);
+
+    for (; 0 <= n; n--)
+    {
+        for (j = 0; j <= length; j++)
+        {
+            dest[j] = dest[j] + src[j];
+        }
+    }
+}
