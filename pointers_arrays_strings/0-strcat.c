db7a73eaeef2a82fb9a668986681f520e42295e9
maelpseudo <mael.cuny@gmail.com>
2023-10-24 11:35:17 +0200

    10

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * 
+ * 
+ * 
+*/
+
+char *_strcat(char *dest, char *src)
+{
+
+    int i, j;
+
+    while(dest[i]!='\0')
+  i++;
+  while(src[j]!='\0')
+  {
+    dest[i]=src[j];
+    j++;
+    i++;
+  }
+  dest[i]='\0';
+}
