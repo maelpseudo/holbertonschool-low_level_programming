d7281e7a9814ba4365cd7ea5d065c53a619bba2b
maelpseudo <mael.cuny@gmail.com>
2023-10-25 13:15:48 +0200

    4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -14,7 +14,6 @@ void reverse_array(int *a, int n) {
     int i;
 
     for (i = 0; i < n / 2; i++) {
-        // Swap the elements from the beginning and end of the array
         temp = a[i];
         a[i] = a[n - i - 1];
         a[n - i - 1] = temp;
