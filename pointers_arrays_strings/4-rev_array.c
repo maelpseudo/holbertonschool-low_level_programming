9ad8383893e61ad335c60c8bafbd34231b5c1dd1
maelpseudo <mael.cuny@gmail.com>
2023-10-25 13:17:32 +0200

    4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,19 +3,21 @@
 #include <string.h>
 
 /**
- * 
- * 
- * 
- * 
+ * reverse_array - a function that reverse an array
+ * @a: input integer
+ * @n: input integer
+ * Return: void
 */
 
-void reverse_array(int *a, int n) {
-    int temp;
-    int i;
+void reverse_array(int *a, int n)
+{
+int temp;
+int i;
 
-    for (i = 0; i < n / 2; i++) {
-        temp = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = temp;
-    }
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = temp;
+}
 }
