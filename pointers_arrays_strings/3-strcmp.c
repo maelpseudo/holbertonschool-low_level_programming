e8294b2a1cd1927aad0327765705b6b4c1381b55
maelpseudo <mael.cuny@gmail.com>
2023-10-25 12:33:07 +0200

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    if (*s1 == *s2)
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+
+    if (len1 == len2)
     {
         return (0);
     }
-    else if (*s1 < *s2)
+    else if (len1 < len2)
     {
         return (-13);
     }
