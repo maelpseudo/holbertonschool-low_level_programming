113e0ab206d7109b0a19c22ba7be62e6e40d3a4b
maelpseudo <mael.cuny@gmail.com>
2023-10-25 12:43:31 +0200

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+
+void reverse_array(int *a, int n)
+{
+    int length = strlen(a);
+    int result;
+
+    for(; length >= 0; length--)
+    {
+        result = result + a[length];
+    }
+
+}
