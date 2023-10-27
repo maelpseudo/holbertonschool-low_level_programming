9a834cb3d6892fce2b2daba969ca91a8abdb0846
maelpseudo <mael.cuny@gmail.com>
2023-10-28 01:07:53 +0200

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * _memcpy - a function that copy src to dest
+ * @dest: pointer to a char
+ * @src: pointer to a char
+ * @n: unsigned int
+ * Return: @dest
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-    return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
