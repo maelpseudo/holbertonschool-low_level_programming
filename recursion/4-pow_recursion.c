6c6e131e9ec141773187b5010cee3e3d1be5cb03
maelpseudo <mael.cuny@gmail.com>
2023-11-01 23:28:22 +0100

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,25 +7,27 @@
  * find_sqrt - function that checks the number's square
  * @x: integer input
  * @y: integer input
- * Return: The square of 'a', otherwise -1
+ * Return: The square of a, otherwise -1
  */
 int find_sqrt(int x, int y)
 {
-	if (y * y == x)
-	{
-		return (y);
-	}
-	else if (y * y > x)
-	{
-		return (-1);
-	}
-	return (find_sqrt(x, y + 1));
+if (y * y == x)
+{
+return (y);
 }
+else if (y * y > x)
+{
+return (-1);
+}
+return (find_sqrt(x, y + 1));
+}
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: input integer
  * Return: natural square of n otherwise -1
  */
+
 int _sqrt_recursion(int n)
 {
 return (find_sqrt(n, 1));
