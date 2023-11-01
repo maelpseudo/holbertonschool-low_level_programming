904205c32ef384fc1347dc308103b5b6958a54f4
maelpseudo <mael.cuny@gmail.com>
2023-11-01 22:42:04 +0100

    recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -12,13 +12,10 @@
 
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return (-1);
-    }
-    else
-    {
-        x = pow(x, y);
-        return (x);
-    }
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x *  _pow_recursion(x, (y - 1)));
 }
