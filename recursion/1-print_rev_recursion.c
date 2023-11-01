c15bc517b230669b36e80047cce717448a3aab4d
maelpseudo <mael.cuny@gmail.com>
2023-11-01 15:26:01 +0100

    recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,24 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * 
- * 
- * 
-*/
+ * _print_rev_recursion - a function that prints a string in reverse
+ * @s: pointer to character
+ * Return: void
+ */
 
-void _print_rev_recursion(char *s, int len) {
-    if (len < 1) {
-        putchar('\n');
-        return;
-    }
-
-    printf("%c", s[len - 1]);
-    _print_rev_recursion(s, len - 1);
-}
-
-void print_reverse(char *s) {
-    int len = strlen(s);
-    _print_rev_recursion(s, len);
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
 }
