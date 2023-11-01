d134d501e696c9a89529d2a252070a900cfe8ad9
maelpseudo <mael.cuny@gmail.com>
2023-11-01 21:37:38 +0100

    recursion

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,16 +7,12 @@
  * 
 */
 
-int factorial(int n) {
-    if (n < 0) {
-        return -1;
-    } else {
-        int result = 1;
-        if (n > 0) {
-            result *= n;
-            n--;
-            factorial(n);
-            return result;
-        }
-    }
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
