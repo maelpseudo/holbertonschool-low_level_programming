a887d60e8824b3734d505a13dbc5a9c8c2992529
maelpseudo <mael.cuny@gmail.com>
2023-11-03 13:56:36 +0100

    2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 
 /**
- * 
- * 
- * 
+ * main - program that prints all arguments it receives
+ * @argc: input integer
+ * @argv: input integer
+ * Return: 0 (Success)
 */
 
 
-int main ()
+int main(int argc, char *argv[])
 {
-
+for (int i = 0; i < argc; i++) {
+printf("%s\n", argv[i]);
+}
+return (0);
 }
