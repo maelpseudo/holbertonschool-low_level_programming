488471ea659ba6f57748980ab63898bdfbaac1ef
maelpseudo <mael.cuny@gmail.com>
2023-11-03 14:24:04 +0100

    4

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,26 +1,42 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - program that multiplies two numbers
+ * main - program that adds positive numbers
  * @argc: input integer
  * @argv: input integer
  * Return: 0 (Success)
 */
 
-
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-if (argc != 3)
+int i, sum = 0;
+int isValid = 1;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+int j = 0;
+while (argv[i][j] != '\0')
+{
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 *num2;
-printf("%d\n", result);
+j++;
+}
+int currentNum = atoi(argv[i]);
+if (currentNum < 0) {
+printf("Error\n");
+return (1);
+}
+sum += currentNum;
+}
+printf("%d\n", sum);
 return (0);
 }
