8ecca841e4d190892da0951b7d0a0300c3d6646e
maelpseudo <mael.cuny@gmail.com>
2023-11-03 14:33:24 +0100

    4

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -3,43 +3,39 @@
 #include <ctype.h>
 
 /**
- * main - program that adds positive numbers
- * @argc: input integer
- * @argv: input integer
- * Return: 0 (Success)
+ * 
+ * 
+ * 
+ * 
 */
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    int sum = 0;
+    int isValid = 1;
 
-int i = 0;
-int sum = 0;
-int isValid = 1;
-if (argc == 1)
-{
-printf("0\n");
-return (0);
-}
-for (i = 1; i < argc; i++)
-{
-int j = 0;
-while (argv[i][j] != '\0')
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
-}
-j++;
-}
-int currentNum = atoi(argv[i]);
-if (currentNum < 0)
-{
-printf("Error\n");
-return (1);
-}
-sum += currentNum;
-}
-printf("%d\n", sum);
-return (0);
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int j = 0;
+        while (argv[i][j] != '\0') {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 1;
+            }
+            j++;
+        }
+
+        int currentNum = atoi(argv[i]);
+        if (currentNum < 0) {
+            printf("Error\n");
+            return 1;
+        }
+        sum += currentNum;
+    }
+
+    printf("%d\n", sum);
+    return 0;
 }
