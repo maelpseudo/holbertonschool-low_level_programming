a78ca4ba7549c4bd72094a85a6390f53909ab582
maelpseudo <mael.cuny@gmail.com>
2023-11-03 13:52:39 +0100

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * 
- * 
- * 
+ * main - program that prints the number of arguments passed into it
+ * @argc: input integer
+ * @argv: input integer
+ * Return: 0 (Success)
 */
 
 
-int main ()
-{
-
+int main(int argc, char *argv[]) {
+    printf("Number of arguments: %d\n", argc - 1);
+    return 0;
 }
