8ada9d63a243979a7c6f3f8655e95edb84eff293
maelpseudo <mael.cuny@gmail.com>
2023-11-03 11:50:53 +0100

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - function that print the name of the file
+ * @argc: input integer
+ * @argv: input integer
+ * Return: 0 (Success)
+*/
+
 int main(int argc, char *argv[]) {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return 0;
+(void)argc;
+printf("%s\n", argv[0]);
+return 0;
 }
