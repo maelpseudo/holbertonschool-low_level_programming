9a52ee44eb422c10b49157de0cc99ae21ff5dd3c
maelpseudo <mael.cuny@gmail.com>
2023-11-03 14:18:47 +0100

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,11 +16,11 @@ int num1, num2, result;
 if (argc != 3)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 result = num1 * num2;
 printf("%d\n", result);
-return 0;
+return (0);
 }
