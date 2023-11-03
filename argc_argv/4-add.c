8ec7b080d4d736b02d232d24a57a41714a8004b6
maelpseudo <mael.cuny@gmail.com>
2023-11-03 14:39:39 +0100

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,32 +14,28 @@
 
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc == 1) {
-printf("0\n");
-return (0);
-}
-int currentNum;
-for (i = 1; i < argc; i++)
-{
-int j = 0;
-while (argv[i][j] != '\0')
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
-}
-j++;
-}
-currentNum = atoi(argv[i]);
-if (currentNum < 0)
-{
-printf("Error\n");
-return (1);
-}
-sum += currentNum;
-}
-printf("%d\n", sum);
-return (0);
+	int i, j;
+	int res = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		res += atoi(argv[i]);
+	}
+	printf("%d\n", res);
+	}
+	return (0);
 }
