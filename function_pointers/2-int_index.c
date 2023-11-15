79b67c7acdb73d9ac9e4bdedd42ff024ac07bb6e
maelpseudo <mael.cuny@gmail.com>
2023-11-15 20:03:02 +0100

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,28 +3,32 @@
 #include "function_pointers.h"
 
 /**
- * 
- * 
- * 
-*/
+ * int_index - function that searches for an integer
+ * @array: array integers
+ * @size: array size
+ * @cmp: pointer 
+ * Return: value
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+int i;
 if (size <= 0)
 {
 return (-1);
-} 
-for (int i = 0; i < size; i++)
-{
-if (cmp(array[i]) != 0)
-{
-return (i);
 }
-}
+if (cmp == NULL)
+{
 return (-1);
 }
-
-int is_even(int num)
+if (array == NULL)
 {
-return (num % 2 == 0 ? 1 : 0);
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
 }
