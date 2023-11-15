dc56523a99c923750548cf8a27a0d155d730e7db
maelpseudo <mael.cuny@gmail.com>
2023-11-15 10:58:12 +0100

    structures_typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,34 +4,30 @@
 
 /**
  * print_dog - function that prints a struct dog
- * @d: struct
+ * @d: pointer to struct
  * Return: void
-*/
+ */
 
 void print_dog(struct dog *d)
 {
-    if (d->name == NULL)
-    {
-    printf("Name: (nil)\n");
-    }
-    else
-    {
-    printf("Name: %s\n", d->name);
-    }
-    if (d->age == NULL)
-    {
-    printf("Age: (nil)\n");
-    }
-    else
-    {
-    printf("Age: %s\n", d->age);
-    }
-    if (d->owner == NULL)
-    {
-    printf("Owner: (nil)\n");
-    }
-    else
-    {
-    printf("Owner: %s\n", d->owner);
-    }
+if (d)
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
 }
