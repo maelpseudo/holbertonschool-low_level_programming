dd3b146583f4e378fd4d37156fa54680840b81b5
maelpseudo <mael.cuny@gmail.com>
2023-11-29 15:51:59 +0100

    4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_list - function that frees a list_t list
+ * @head: The pointer to the first node of linked list
+ * Return: void
+ */
+
 void free_list(list_t *head)
 {
-    list_t *temp;
+list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head->next;
-        free(head->str);
-        free(head);
-        head = temp;
-    }
+while (head != NULL)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
 }
