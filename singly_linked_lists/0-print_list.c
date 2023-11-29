45ecee1fbdc1ace71668c713574b2040affbb94b
maelpseudo <mael.cuny@gmail.com>
2023-11-29 15:53:52 +0100

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,18 +10,18 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t nodes = 0;
+size_t nodes = 0;
 
-    while (h)
-    {
-        if (h->str)
-            printf("[%u] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
+while (h)
+{
+if (h->str)
+printf("[%u] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
 
-        nodes++;
-        h = h->next;
-    }
+nodes++;
+h = h->next;
+}
 
-    return nodes;
+return nodes;
 }
