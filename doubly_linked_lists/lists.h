93fdc7121a9dc45d2719a9d6e9a1d8ef1d1d6400
maelpseudo <mael.cuny@gmail.com>
2023-12-01 10:26:17 +0100

    0 + README.md

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif // MAIN_H
