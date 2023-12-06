1b0fc11a54ec9d990e7e7d551389eb5c5f7794ae
maelpseudo <mael.cuny@gmail.com>
2023-12-06 15:13:44 +0100

    updated main.h and added task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int create_file(const char *filename, char *text_content)
+{
+    if (filename == NULL)
+    {
+        return -1;
+    }
+
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd == -1)
+    {
+        return -1;
+    }
+
+    if (text_content != NULL)
+    {
+        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+
+    return 1;
+}
