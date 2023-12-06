c818e4e26b9775f4cf53a308e62b363859a4629f
maelpseudo <mael.cuny@gmail.com>
2023-12-06 15:45:00 +0100

    updated task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,39 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * 
- * 
- * 
- * 
-*/
+ * create_file - a function that creates a file
+ * @filename: the name of the file
+ * @text_content: a NULL terminated string to write to the file
+ * Return: if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-    {
-        return (-1);
-    }
+	int fd, j;
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-    {
-        return (-1);
-    }
-
-    if (text_content != NULL)
-    {
-        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-
-    close(fd);
-
-    return (1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (j = 0; text_content[j] != '\0'; j++)
+		{
+		}
+		write(fd, text_content, j);
+	}
+	close(fd);
+	return (1);
 }
