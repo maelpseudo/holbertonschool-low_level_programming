dfe28eae1c5eab61a5ad7e284b8de220e8e2985b
maelpseudo <mael.cuny@gmail.com>
2024-01-16 16:38:17 +0100

    Updated 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,26 +1,20 @@
 #include "search_algos.h"
 
-/**
- * 
- * 
- * 
- * 
-*/
-
 int linear_search(int *array, size_t size, int value)
 {
+
         size_t i;
 
-        if (array == NULL)
+        if (array == NULL) {
                 return -1;
+        }
 
-        for (i = 0; i < size; i++)
-        {
-                printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-                if (value == array[i])
-                {
+        for (i = 0; i < size; i++) {
+                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+                if (array[i] == value) {
                         return i;
                 }
         }
+
         return -1;
 }
