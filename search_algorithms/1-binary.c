ed2ffb1615161a326476391eb327894537faaf19
maelpseudo <mael.cuny@gmail.com>
2024-01-17 09:26:45 +0100

    Updated 1-binary.c with betty

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -3,42 +3,48 @@
 
 /**
  * binary_search - Searches for a value in a sorted array of integers
- *                 using the Binary search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for
- * Return: The index where value is located, or -1 if not found or array is NULL
+ * using the Binary search algorithm.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in array.
+ * @value: Value to search for.
+ * Return: The index where value is located, or -1 if not found or array is NULL.
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-        size_t left = 0;
-        size_t right = size - 1;
-        size_t mid, i;
+size_t left = 0;
+size_t right = size - 1;
+size_t mid, i;
 
-        if (!array)
-                return -1;
+if (!array)
+return -1;
 
-        while (left <= right)
-        {
-                printf("Searching in array: ");
-                for (i = left; i <= right; i++)
-                {
-                printf("%d", array[i]);
-                if (i < right)
-                        printf(", ");
-                }
-                printf("\n");
+while (left <= right)
+{
+printf("Searching in array: ");
+for (i = left; i <= right; i++)
+{
+printf("%d", array[i]);
+if (i < right)
+printf(", ");
+}
+printf("\n");
 
-                mid = left + (right - left) / 2;
+mid = left + (right - left) / 2;
 
-                if (array[mid] == value)
-                        return mid;
-                else if (array[mid] < value)
-                        left = mid + 1;
-                else
-                        right = mid - 1;
-        }
+if (array[mid] == value)
+{
+return mid;
+}
+else if (array[mid] < value)
+{
+left = mid + 1;
+}
+else
+{
+right = mid - 1;
+}
+}
 
-        return -1;
+return -1;
 }
